c8ca810b00448f1402834b87371dd71a2d21719a
richmond adusei <richmond.nana2000@gmail.com>
2026-01-25 23:42:41 +0000

    bookshelf

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -8,3 +8,30 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     list_filter = ('publication_year', 'author')
     search_fields = ('title', 'author')
+
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Information", {
+            "fields": ("date_of_birth", "profile_photo"),
+        }),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional Information", {
+            "fields": ("date_of_birth", "profile_photo"),
+        }),
+    )
+
+    list_display = ("username", "email", "date_of_birth", "is_staff", "is_active")
+    search_fields = ("username", "email")
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
